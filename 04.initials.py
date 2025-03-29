782b93577e4e4f72233cb458d4a7b1bdb53f4f08
Surya KP <127583162+SuryaKPog@users.noreply.github.com>
2025-03-29 14:56:13 +0530

    Create 04.initials.py

---
+++ 04.initials.py
@@ -0,0 +1,11 @@
+print("K  K    P P P")
+print("K K     P   P")
+print("K       P P P")
+print("K K     P")
+print("K  K    P")
+
+#output: K  K    P P P
+#        K K     P   P
+#        K       P P P
+#        K K     P
+#        K  K    P
