3e0ac7f5ac587892180bff9922d432c79003608b
Surya KP <127583162+SuryaKPog@users.noreply.github.com>
2025-03-29 14:59:03 +0530

    Create 05.temperature.py

---
+++ 05.temperature.py
@@ -0,0 +1,15 @@
+#Create a temperature.py program that converts a number from Fahrenheit (°F) to Celsius (°C).
+
+#Google the current temperature of Brooklyn, NY (where Codédex is based) in °F.
+
+#Use the following formula and write it out in Python:
+#Celsius= 1.8(Fahrenheit−32)
+
+Fahr=60
+Cel=(Fahr-32)/1.8
+print(Cel)
+
+#output: 15.555555555
+
+​
+ 
