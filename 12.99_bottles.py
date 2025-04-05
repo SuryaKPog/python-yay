a97031968d63a9b73c5d305f4262d289db479692
Surya KP <127583162+SuryaKPog@users.noreply.github.com>
2025-04-05 11:29:49 +0530

    Create 12.99_bottles.py

---
+++ 12.99_bottles.py
@@ -0,0 +1,11 @@
+#Using String interpolation is a process of substituting values of variables into placeholders in a string.
+
+#For instance, if you have a template for saying hello to a person in an email like 'Hi {name}, nice to meet you!', you would like to replace the placeholder {name} with an actual name. This is string interpolation.
+
+# String interpolation
+
+for i in range(99,0,-1):
+  print(f"{i} bottles of beer on the wall")
+  print(f"{i} bottles of beer")
+  print("Take one down, pass it around")
+  print(f"{i-1} bottles of beer on the wall")
