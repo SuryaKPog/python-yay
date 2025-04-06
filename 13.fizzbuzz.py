63aa98eb7080346e54149b6bce455824165c76cf
Surya KP <127583162+SuryaKPog@users.noreply.github.com>
2025-04-06 16:42:20 +0530

    Update 13.fizzbuzz.py

--- 13.fizzbuzz.py
+++ 13.fizzbuzz.py
@@ -2,6 +2,7 @@
 #For multiples of 3, print "Fizz" instead of the number
 #For multiples of 5, print "Buzz" instead of the number
 #For multiples of 3 and 5, print "FizzBuzz"
+#im gonna
 
 for i in range(1,101):
   if i%3==0 and i%5==0:
